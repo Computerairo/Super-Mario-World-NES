4bda10f805889c7b4457aa0e4c37d70638da8671
Computerairo <computerairo@gmail.com>
2024-10-23 20:07:38 +0200

    Changed background color

--- SuperMarioWorld.asm
+++ SuperMarioWorld.asm
@@ -8,7 +8,7 @@
 
 ;;;;; NES CARTRIDGE HEADER
 
-	NES_HEADER 0,2,1,NES_MIRR_HORIZ ; mapper 0, 2 PRGs, 1 CHR
+	NES_HEADER 0,2,6,NES_MIRR_HORIZ ; mapper 0, 2 PRGs, 1 CHR
 
 ;;;;; START OF CODE
 
@@ -16,13 +16,13 @@ Start:	subroutine
 	NES_INIT	; set up stack pointer, turn off PPU
         jsr WaitSync	; wait for VSYNC
         jsr ClearRAM	; clear RAM
-        jsr WaitSync	; wait for VSYNC (and PPU warmup)
+        jsr WaitSync ; wait for VSYNC (and PPU warmup)
 
 	lda #$3f	; $3F -> A register
         ldy #$00	; $00 -> Y register
         sta PPU_ADDR	; write high byte first
         sty PPU_ADDR    ; $3F00 -> PPU address
-        lda #$1c	; $1C = light blue color
+        lda #$2C	; $1C = light blue color
         sta PPU_DATA    ; $1C -> PPU data
         lda #CTRL_NMI
         sta PPU_CTRL	; enable NMI
